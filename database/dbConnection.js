a9daaec7d4aea8ec2135d9c6d4e1617d0038b144
Gajarthan <31730712+Gajarthan@users.noreply.github.com>
2024-11-10 22:55:47 +0530

    first commit

---
+++ database/dbConnection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+    mongoose.connect(process.env.MONGO_URL, {
+        dbName: "REACT_HOSPITAL_MANAGEMENET_SYSTEM",
+    })
+    .then(() => {
+        console.log("Connected to database");
+    })
+    .catch((error) => {
+        console.log(error);
+        console.log("Error occured while connecting to database");
+    });
+};
+
